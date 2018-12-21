151daf998aab28bd8308d199e7d831cbe0dcebfb
Felipe Velasquez <felipe.velasquez@globant.com>
2018-12-21 16:33:36 -0300

    All working

--- src/client.js
+++ src/client.js
@@ -6,26 +6,31 @@ const PORT = 50051;
 const server = new grpc.Server();
 const SERVER = '104.197.113.13:443';//'0.0.0.0:50051'; //
 trainModel.getModel().then((model) => {
-    var client = new model.Train(`${SERVER}`,
-        grpc.credentials.createInsecure());
-    const messages = model.messages;
-    const newModel = new messages.Model();
-    const text = new messages.Text();
-    const catallog = process.argv[2];
-    newModel.setName(catallog)
-    text.setModelname(catallog);
-    text.setText(process.argv[3]);
-    let time1 = new Date();
-    client.train(newModel.toObject(), (err, response) => {
-        console.log(err);
-        console.log(response);
+    try {
+        var client = new model.Train(`${SERVER}`,
+            grpc.credentials.createInsecure());
+        const messages = model.messages;
+        const newModel = new messages.Model();
+        const text = new messages.Text();
+        const catallog = process.argv[2];
+        newModel.setName(catallog)
+        text.setModelname(catallog);
+        text.setText(process.argv[3]);
+        let time1 = new Date();
+        //client.train(newModel.toObject(), (err, response) => {
+        //  console.log(err);
+        //  console.log(response);
         client.classify(text.toObject(), (err, response) => {
             console.log(err);
             console.log(response);
             let time2 = new Date();
             console.log(`in ${time2 - time1} ms`, response);
         });
-    });
+        //});
+    }
+    catch (ex) {
+        console.log(ex);
+     }
 })
 /*
     const messages = model.messages;
